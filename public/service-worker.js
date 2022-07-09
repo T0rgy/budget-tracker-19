d6f484d1d2e08bb3f41befe07485679b53fcefe6
Torgy <jctorge@gmail.com>
2022-07-08 21:39:05 -0500

    service worker rework

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,7 @@
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
@@ -15,74 +19,52 @@ const FILES_TO_CACHE = [
     '/manifest.json'
 ];
 
-const STATIC_CACHE = "static-cache-v2";
-const DATA_CACHE = "data-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
 
-// lifecylce method
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(STATIC_CACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+// installing the service worker
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
     );
 });
 
-// old cache clean up
-self.addEventListener('activate', event => {
-    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-        })
-        .then(deleteCaches => {
-            return Promise.all(deleteCaches.map(deleteCaches => {
-                return chaches.delete(deleteCaches);
-            }));
-        })
-        .then(() => self.clients.claim())
-    );
-});
-
-// fetch
-self.addEventListener('fetch', event => {
-    if ( event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(fetch(event.request));
-        return;
-    }
-
-
-if (event.request.url.includes('/api/transaction')) {
-    event.respondWith(
-        caches.open(RUNTIME_CACHE).then(cache => {
-            return fetch(event.request)
-            .then(response => {
-                cache.put(event.request, response.clone());
-                return response;
-            })
-            .catch(() => caches.match(event.request));
-        })
-    );
-    return;
-}
-
-
-event.respondWith(
-    caches.match(event.request)
-    .then(cacheResponse => {
-        if (cacheResponse) {
-            return cacheResponse;
-        }
-
-        return caches.open(RUNTIME_CACHE)
-        .then(cache => {
-            return fetch(event.request)
-            .then(response => {
-                return cache.put(event.request, response.clone())
-                .then(() => {
-                    return response;
+// activating service worker
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // filter out ones that has this app prefix to create keeplist
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
                 });
-            });
-        });
-    })
-)});
+                // add current cache name to keeplist
+                cacheKeepList.push(CACHE_NAME);
+
+                // returns promise that resolves once all old versions of cache have been delete
+                return Promise.all(keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[1]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    )
+});
+
+// recieving info from cache
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
